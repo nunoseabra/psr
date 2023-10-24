69da35ce46fbeaec8d9825268b12a0e538abe096
nunoseabra <145006257+nunoseabra@users.noreply.github.com>
2023-10-24 18:54:07 +0100

    Delete parte 2 directory

--- parte 2/ex4/main.py
+++
@@ -1,9 +0,0 @@
-from print import printAllCharsUpTo, readAllUpTo, countNumbersUpto
-
-def main():
-    printAllCharsUpTo()
-    readAllUpTo('x')  
-    countNumbersUpto('x')
-
-if __name__ == '__main__':
-    main()
